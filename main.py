cd2371c15473baef96504e4ab4b8a3ba3bf78fab
Vinoo <ivinoop@gmail.com>
2024-01-06 13:59:05 +0530

    add generate report functionality; refactor w/ solution

--- main.py
+++ main.py
@@ -2,9 +2,30 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"There are {num_words} words in the book")
+    # print(f"There are {num_words} words in the book")
     num_letters = get_letters(text)
-    print(num_letters)
+    # print(num_letters)
+    report_list = dict_to_list(num_letters)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in report_list:
+        if not item["letter"].isalpha():
+            continue
+        print(f"The '{item['letter']}' character was found {item['count']} times")
+
+    print("--- End report ---")
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
 
 def get_letters(text):
@@ -18,14 +39,21 @@ def get_letters(text):
     return letters
 
 
-def get_num_words(text):
-    words = text.split()
-    return len(words)
+def dict_key(book_dict):
+  return book_dict["count"]
 
 
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
+def dict_to_list(book_dict):
+  report_list =[]
+  for i in book_dict:
+    report_list.append({"letter": i, "count": book_dict[i]})
+  report_list.sort(reverse=True, key=dict_key)
+  return report_list
+
+
+
+
+
 
 
 main()
